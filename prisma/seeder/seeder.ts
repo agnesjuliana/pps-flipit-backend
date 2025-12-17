03bd01d7817c70b59b0f81318f046772e3522470
Eugenia Indrawan <122144767+nerdypengu@users.noreply.github.com>
2025-12-18 00:21:03 +0700

    fix : seeder logic

--- prisma/seeder/seeder.ts
+++ prisma/seeder/seeder.ts
@@ -1,77 +1,58 @@
-// import { PrismaClient } from '@prisma/client';
-// import csv from 'csvtojson';
-// import * as bcryptjs from 'bcryptjs';
+import { PrismaClient, EducationLevel, Role } from '@prisma/client';
+import csv from 'csvtojson';
+import * as bcryptjs from 'bcryptjs';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// const salt = bcryptjs.genSaltSync(12);
+const salt = bcryptjs.genSaltSync(12);
 
-// async function users() {
-//   const dataUser = await csv().fromFile(__dirname + '/data/users.csv');
-//   let users = dataUser.map(user => {
-//     return {
-//       email: user.email,
-//       password: bcryptjs.hashSync(user.password, salt),
-//       role: user.role,
-//       name: user.name,
-//       is_email_verified: user.is_email_verified == 'true' ? true : false
-//     };
-//   });
+async function users() {
+  const dataUser = await csv().fromFile(__dirname + '/data/users.csv');
+  let users = dataUser.map(user => {
+    return {
+      email: user.email,
+      password: bcryptjs.hashSync(user.password, salt),
+      role: user.role as Role,
+      name: user.name,
+      dateOfBirth: new Date('2000-01-01'),
+      educationLevel: EducationLevel.Undergraduate,
+    };
+  });
 
-//   for (const user of users) {
-//     if (user.role == 'ADMIN') {
-//       await prisma.accounts.upsert({
-//         where: {
-//           email: user.email,
-//         },
-//         update: {
-//           email: user.email,
-//           is_email_verified: user.is_email_verified,
-//           password: user.password,
-//           role: user.role,
-//           name: user.name,
-//         },
-//         create: {
-//           email: user.email,
-//           is_email_verified: user.is_email_verified,
-//           password: user.password,
-//           role: user.role,
-//           name: user.name,
-//         },
-//       });
-//     } else {
-//       await prisma.accounts.upsert({
-//         where: {
-//           email: user.email,
-//         },
-//         update: {
-//           email: user.email,
-//           is_email_verified: user.is_email_verified,
-//           password: user.password,
-//           role: user.role,
-//           name: user.name
-//         },
-//         create: {
-//           email: user.email,
-//           is_email_verified: user.is_email_verified,
-//           password: user.password,
-//           role: user.role,
-//           name: user.name,
-//         },
-//       });
-//     }
-//   }
-// }
+  for (const user of users) {
+    await prisma.user.upsert({
+      where: {
+        email: user.email,
+      },
+      update: {
+        email: user.email,
+        password: user.password,
+        role: user.role,
+        name: user.name,
+        dateOfBirth: user.dateOfBirth,
+        educationLevel: user.educationLevel,
+      },
+      create: {
+        email: user.email,
+        password: user.password,
+        role: user.role,
+        name: user.name,
+        dateOfBirth: user.dateOfBirth,
+        educationLevel: user.educationLevel,
+      },
+    });
+  }
+}
 
-// const main = async () => {
-//   await users();
-// };
+const main = async () => {
+  await users();
+};
 
-// main()
-//   .catch(e => {
-//     console.error(e);
-//     process.exit(1);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
